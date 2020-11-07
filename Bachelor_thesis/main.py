99b961c34b1f522737eddcfb0b97dba2ade69e75
TomasPetricek <tomas.petricek@laststar.eu>
2020-11-07 18:20:59 +0100

    Add PyHTK

--- Bachelor_thesis/main.py
+++ Bachelor_thesis/main.py
@@ -1,5 +1,7 @@
-from preprocessing.convertors import AudioFormatConverter
+from processing.convertors import AudioFormatConverter
 from classes import Dataset
+from libraries.PyHTK.HTK import HTKFile
+from pprint import pprint
 
 
 DATASET_PATH = "/Users/tomaspetricek/TUL/TUL_2020:21/BP/Speech_Emotion_Recognition/Datasets/{language}/{name}/{" \
@@ -7,55 +9,61 @@ DATASET_PATH = "/Users/tomaspetricek/TUL/TUL_2020:21/BP/Speech_Emotion_Recogniti
 
 
 if __name__ == "__main__":
+    htk_file = HTKFile()
+    htk_file.load(filename='/Users/tomaspetricek/TUL/TUL_2020:21/BP/Test/03-01-01-01-01-01-01.mfc')
 
-    language_ = "italian"
-    name_ = "EMOVO"
+    print(type(htk_file.data))
+    for data in htk_file.data:
+        pprint(data)
 
-    original_dataset = Dataset(
-        path=DATASET_PATH.format(
-            language=language_,
-            name=name_,
-            form="original"
-        )
-    )
-
-    # # change dataset files permissions
-    # change_permissions(
-    #     files=original_dataset.samples,
-    #     permission=755
-    # )
-
-    # samples_ = original_dataset.samples
-    # for sample in samples_:
-    #     print(sample)
+    # language_ = "italian"
+    # name_ = "EMOVO"
     #
-    # print(len(samples_))
-
-    # converted_dataset = original_dataset.clone(
-    #     clone_path=DATASET_PATH.format(
+    # original_dataset = Dataset(
+    #     path=DATASET_PATH.format(
+    #         language=language_,
+    #         name=name_,
+    #         form="original"
+    #     )
+    # )
+    #
+    # # # change dataset files permissions
+    # # change_permissions(
+    # #     files=original_dataset.samples,
+    # #     permission=755
+    # # )
+    #
+    # # samples_ = original_dataset.samples
+    # # for sample in samples_:
+    # #     print(sample)
+    # #
+    # # print(len(samples_))
+    #
+    # # converted_dataset = original_dataset.clone(
+    # #     clone_path=DATASET_PATH.format(
+    # #         language=language_,
+    # #         name=name_,
+    # #         form="converted"
+    # #     )
+    # # )
+    #
+    # converted_dataset = Dataset(
+    #     path=DATASET_PATH.format(
     #         language=language_,
     #         name=name_,
     #         form="converted"
     #     )
     # )
-
-    converted_dataset = Dataset(
-        path=DATASET_PATH.format(
-            language=language_,
-            name=name_,
-            form="converted"
-        )
-    )
-
-    converter = AudioFormatConverter(
-        input_files=original_dataset.samples,
-        output_files=converted_dataset.samples,
-        audio_channel=AudioFormatConverter.MONO,
-        sample_rate=AudioFormatConverter._16KHz
-    )
-
-    # start = timer()
-    converter.convert()
-    # end = timer()
-    # print("time: {:>8.2f}s".format(end - start))    # RAVDESS time: 54.56s, SAVEE time: 23.40s, TESS time: 108.75s
+    #
+    # converter = AudioFormatConverter(
+    #     input_files=original_dataset.samples,
+    #     output_files=converted_dataset.samples,
+    #     audio_channel=AudioFormatConverter.MONO,
+    #     sample_rate=AudioFormatConverter._16KHz
+    # )
+    #
+    # # start = timer()
+    # converter.convert()
+    # # end = timer()
+    # # print("time: {:>8.2f}s".format(end - start))    # RAVDESS time: 54.56s, SAVEE time: 23.40s, TESS time: 108.75s
 
