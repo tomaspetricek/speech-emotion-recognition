ad93c807a07562fb11f36a90fcdbb40a48fb7b6e
TomasPetricek <tomas.petricek@laststar.eu>
2020-11-08 14:50:20 +0100

    Add RAVDESS MFCC

--- Bachelor_thesis/main.py
+++ Bachelor_thesis/main.py
@@ -9,34 +9,32 @@ from utils import change_file_extension
 
 if __name__ == "__main__":
 
-    language_ = "italian"
-    name_ = "EMOVO"
+    language_ = "english"
+    name_ = "RAVDESS"
 
     original_dataset = Dataset(
         path=DATASET_PATH.format(
             language=language_,
             name=name_,
-            form="original"
+            form="converted"
         )
     )
 
-    # converted_dataset = original_dataset.clone(
-    #     clone_path=DATASET_PATH.format(
-    #         language=language_,
-    #         name=name_,
-    #         form="mfcc",
-    #     ),
-    #     ignore_file_extensions=['.wav']
-    # )
-    #
-    # print(converted_dataset.samples)
+    converted_dataset = original_dataset.clone(
+        clone_path=DATASET_PATH.format(
+            language=language_,
+            name=name_,
+            form="mfcc",
+        ),
+        ignore_file_extensions=['.wav']
+    )
 
     input_files = original_dataset.samples
 
     output_files_ = change_file_extension(input_files, ".mfcc_0_d_a")
     output_files = []
     for output_file in output_files_:
-        output_files.append(output_file.replace("original", "mfcc"))
+        output_files.append(output_file.replace("converted", "mfcc"))
 
     print(len(input_files))
     print(len(output_files))
