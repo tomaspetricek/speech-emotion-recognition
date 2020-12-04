7b4cb457c2f7b1beaa22872fafb6cdd6db2da857
TomasPetricek <tomas.petricek@laststar.eu>
2020-12-04 17:26:10 +0100

    Add test_TESSLabel_parse

--- Test/test_labels.py
+++ Test/test_labels.py
@@ -7,8 +7,13 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from Bachelor_thesis.datasets import TESSLabel
 
-print(TESSLabel)
 
-def test_TESSLabel_parse():
-    pass
+@pytest.mark.parametrize('path, expected_label', [
+    ("/Users/tomaspetricek/TUL/TUL_2020:21/BP/Speech_Emotion_Recognition/Datasets/english/TESS/mfcc/OAF_angry/OAF_back_angry.mfcc_0_d_a", ['OAF', 'back', 'angry']),
+    ("/Users/tomaspetricek/TUL/TUL_2020:21/BP/Speech_Emotion_Recognition/Datasets/english/TESS/mfcc/OAF_Pleasant_surprise/OAF_back_ps.mfcc_0_d_a", ['OAF', 'back', 'ps']),
+])
+def test_TESSLabel_parse(path, expected_label):
+    label = TESSLabel()
+    assert expected_label == label.parse(path)
+
 
