6af362a0df1010853be35b9e9ed1f6f9b1d736c2
TomasPetricek <tomas.petricek@laststar.eu>
2021-01-07 13:39:12 +0100

    Add models

--- Bachelor_thesis/classifiers.py
+++ Bachelor_thesis/classifiers.py
@@ -1,34 +1,113 @@
-import torch
+from typing import Any
+
 from torch import nn
-import torch.nn.functional as F
+import torch
 
 
-class ClassifierModule(nn.Module):
+# class Classifier(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#
+#         n_input_neurons = 784
+#         n_hidden_neurons = [300, 100]
+#         n_output_neurons = 10
+#
+#         self.hidden_layer = nn.Linear(n_input_neurons, n_hidden_neurons)
+#         self.output_layer = nn.Linear(n_hidden_neurons, n_output_neurons)
+#
+#         self.sigmoid = nn.Sigmoid()
+#         self.softmax = nn.Softmax(dim=1)
+#
+#     def forward(self, X):
+#         X = self.hidden_layer(X)
+#         X = self.sigmoid(X)
+#         X = self.output_layer(X)
+#         X = self.softmax(X)
+#         return X
 
-    # ASI NEPOUZIVAT TENTO PŘÍKLAD. PODÍVAT SE NA JINÝ.
-    """
-    Source: https://colab.research.google.com/github/skorch-dev/skorch/blob/master/notebooks/Basic_Usage.ipynb#scrollTo=N_sDVyu-gASJ
-    """
-    def __init__(
-            self,
-            num_units=10,
-            nonlin=F.relu,
-            dropout=0.5,
-    ):
-        super(ClassifierModule, self).__init__()
-        self.num_units = num_units
-        self.nonlin = nonlin
-        self.dropout = dropout
 
-        self.dense0 = nn.Linear(20, num_units)
-        self.nonlin = nonlin
-        self.dropout = nn.Dropout(dropout)
-        self.dense1 = nn.Linear(num_units, 10)
-        self.output = nn.Linear(10, 2)
+class Sequential(nn.Sequential):
 
-    def forward(self, X, **kwargs):
-        X = self.nonlin(self.dense0(X))
-        X = self.dropout(X)
-        X = F.relu(self.dense1(X))
-        X = F.softmax(self.output(X), dim=-1)
-        return X
+    def _train(self, train_loader, optimizer, criterion):
+        # set model to training mode
+        self.train()
+
+        correct = 0
+        running_loss = 0.0
+        n_samples = len(train_loader.dataset)
+
+        for X, y in train_loader:
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward propagation
+            y_pred = self(X)
+
+            loss = criterion(y_pred, y)
+
+            # back propagation
+            loss.backward()
+
+            optimizer.step()
+
+            # statistics
+            _, y_pred_label = torch.max(y_pred, 1)
+            correct += (y_pred_label == y).sum().item()
+            running_loss += loss.item() * X.shape[0]
+
+        loss_ = running_loss / n_samples
+        accuracy = correct / n_samples
+
+        return loss_, accuracy
+
+    def _eval(self, val_loader, criterion):
+        # set module to evaluation mode
+        self.eval()
+
+        correct = 0
+        running_loss = 0.0
+        n_samples = len(val_loader.dataset)
+
+        with torch.no_grad():
+            for X, y in val_loader:
+                # forward propagation
+                y_pred = self(X)
+
+                loss = criterion(y_pred, y)
+
+                # statistics
+                _, y_pred_label = torch.max(y_pred, 1)
+                correct += (y_pred_label == y).sum().item()
+                running_loss += loss.item() * X.shape[0]
+
+        loss_ = running_loss / n_samples
+        accuracy = correct / n_samples
+
+        return loss_, accuracy
+
+    def fit(self, train_loader, val_loader, criterion, optimizer, n_epochs=10):
+        """
+        Inspiration:
+        https://nodata.science/no-fit-comparing-high-level-learning-interfaces-for-pytorch.html
+        """
+        for epoch in range(n_epochs):
+            print(f"Epoch {epoch + 1}/{n_epochs}")
+
+            # train model
+            train_loss, train_accuracy = self._train(train_loader, optimizer, criterion)
+
+            # evaluate model
+            val_loss, val_accuracy = self._eval(val_loader, criterion)
+
+            # shows stats
+            print(
+                f"train_loss: {train_loss:0.3f} - train_accuracy: {train_accuracy:0.3f}",
+                f" - val_loss: {val_loss:0.3f} - val_accuracy: {val_accuracy:0.3f}"
+            )
+
+        print('Finished Training')
+
+
+
+
+
