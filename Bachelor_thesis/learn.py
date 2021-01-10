d90873c6c85abe4f6afef8283f0219106b64f694
TomasPetricek <tomas.petricek@laststar.eu>
2021-01-10 16:40:42 +0100

    Add cuda checkers

--- Bachelor_thesis/learn.py
+++ Bachelor_thesis/learn.py
@@ -88,11 +88,9 @@ def learn(index_picker, hidden_sizes, batch_size, learning_rate, n_epochs):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Runs on device: {}".format(device))
 
+    # show cuda info
     if device.type == 'cuda':
         print(torch.cuda.get_device_name(0))
-        print('Memory Usage:')
-        print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), 'GB')
-        print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1), 'GB')
 
     # add margin
     X_margined = np.array(add_margin(X, index_picker))
