a5388cc423bebe3f97e03774f3fc3eceee38137c
TomasPetricek <tomas.petricek@laststar.eu>
2021-02-13 14:08:28 +0100

    Add plotting

--- Bachelor_thesis/train.py
+++ Bachelor_thesis/train.py
@@ -6,11 +6,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
 from classifiers import Sequential
 from pytorch_datasets import NumpySampleDataset, NumpyFrameDataset
 from files import DatasetInfoFile, SetInfoFile
-from tools import IndexPicker
 
 ENABLE_LOGGING = True
 
@@ -23,6 +23,26 @@ if ENABLE_LOGGING:
     logger.addHandler(handler)
     sys.stdout.write = logger.info
 
+def plot_history(history):
+    # plot accuracy
+    plt.plot(history['train_accuracy'])
+    plt.plot(history['val_accuracy'])
+    plt.plot(history['test_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val', 'test'], loc='upper left')
+    plt.show()
+
+    # plot loss
+    plt.plot(history['train_loss'])
+    plt.plot(history['val_loss'])
+    plt.plot(history['test_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val', 'test'], loc='upper left')
+    plt.show()
 
 def create_model(input_size, hidden_sizes, output_size):
     input_layer = [
@@ -50,7 +70,7 @@ class Trainer:
         self.val_dataset = val_dataset
         self.test_dataset = test_dataset
 
-    def __call__(self, batch_size, learning_rate, n_epochs):
+    def __call__(self, batch_size, learning_rate, n_epochs, result_path=None):
 
         # set device
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -77,7 +97,21 @@ class Trainer:
         optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
 
         # fit model
-        self.model.fit(train_loader, self.val_dataset, self.test_dataset, criterion, optimizer, device, n_epochs)
+        history = self.model.fit(train_loader, self.val_dataset, self.test_dataset, criterion, optimizer, device,
+                                 n_epochs)
+
+        # save results
+        if result_path:
+            self._save_results(result_path)
+
+        # show results
+        self._show_results(history)
+
+    def _save_results(self, result_path):
+        torch.save(self.model, result_path)
+
+    def _show_results(self, history):
+        plot_history(history)
 
 
 def prepare_dataset(directory, dataset_class, left_margin, right_margin):
@@ -92,9 +126,9 @@ def prepare_dataset(directory, dataset_class, left_margin, right_margin):
 
 
 def main():
-    dataset_dir = "prepared_data/fullset_npy_3"
+    dataset_dir = "prepared_data/fullset_npy_80_20"
 
-    left_margin = right_margin = 25
+    left_margin = right_margin = 50
 
     info_path = os.path.join(dataset_dir, "info.txt")
     n_features, n_classes, n_samples = DatasetInfoFile(info_path).read()
@@ -102,7 +136,7 @@ def main():
     train_dir = os.path.join(dataset_dir, "train")
     train_dataset = prepare_dataset(train_dir, NumpyFrameDataset, left_margin, right_margin)
 
-    val_dir = os.path.join(dataset_dir, "val")
+    val_dir = os.path.join(dataset_dir, "test")
     val_dataset = prepare_dataset(val_dir, NumpySampleDataset, left_margin, right_margin)
 
     # test_dir = os.path.join(dataset_dir, "test")
@@ -114,7 +148,7 @@ def main():
     input_size = n_features * (left_margin + 1 + right_margin)
     model = create_model(
         input_size=input_size,
-        hidden_sizes=[128, 128, 128],
+        hidden_sizes=[64, 64, 64],
         output_size=n_classes
     )
 
@@ -125,7 +159,7 @@ def main():
         test_dataset=test_dataset
     )
 
-    trainer(batch_size=512, learning_rate=0.0001, n_epochs=30)
+    trainer(batch_size=512, learning_rate=0.0001, n_epochs=5, result_path="models/pytorch/model.pt")
 
 
 if __name__ == "__main__":
